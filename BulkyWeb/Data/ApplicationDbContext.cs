3eeabfb1cc6fac5761bb6d6f6af978905b333a4d
JOHNSON26934 <JOHNSON26934@ITDLTPA26934>
2023-11-21 09:25:55 +0100

    Add project files.

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+    
+
+}
